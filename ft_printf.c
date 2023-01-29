a717edb2aec09292f19dce6a88d4bdf985466a89
DMKael <hyungdki@student.42seoul.kr>
2023-01-29 21:36:33 +0900

    .

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,22 @@
-#include "ft_printf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmkael <hyungdki@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 09:47:05 by dmkael            #+#    #+#             */
+/*   Updated: 2023/01/28 09:47:09 by dmkael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_printf(const char *format, ...)
+#include "ft_printf.h"
+#include "libft/libft.h"
+
+int	ft_printf(const char *format, ...)
 {
-    va_list arg;
-    va_start(arg, format);
+	va_list	arg;
+
+	va_start(arg, format);
+	va_end(arg);
 }
