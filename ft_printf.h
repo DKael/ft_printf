30fd7e468758bdb2bc9428cb05a0d3ebd4d9c9d4
Hyungdo Kim <hyungdki@c5r4s5.42seoul.kr>
2023-01-29 21:20:40 +0900

    .

--- ft_printf.h
+++ ft_printf.h
@@ -9,10 +9,18 @@
 /*   Updated: 2023/01/28 09:47:16 by dmkael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# define SHARP_FLAG	1
+# define BLANK_FLAG	2
+# define MINUS_FLAG	4
+# define PLUS_FLAG	8
+# define ZERO_FLAG	16
+
 # include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
 
 #endif
